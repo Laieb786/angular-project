19593f95abecb509f07521a7055de3d9fa27dfd4
Laieb786 <laieb.ahmed@gmail.com>
2025-09-06 19:27:52 +0200

    Extract Constant in a seperated Folder named "constants" for best-practice

---
+++ src/app/_shared/constants/random-index.const.ts
@@ -0,0 +1,3 @@
+import {DUMMY_USERS} from '../../dummy-users';
+
+export const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DUMMY_USERS} from '../dummy-users';
+import {randomIndex} from '../_shared/constants/random-index.const'
 
 @Component({
   selector: 'app-user',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar
+  }
+
+  onSelectedUser() {
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
